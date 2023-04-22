10118ccdb1ef1278887aca3c607fec214f70603e
疾风剑豪 <115929650+sengezuishuai@users.noreply.github.com>
2023-04-22 21:11:06 +0800

    Add files via upload

---
+++ tryLoseOf/tryLoseOf/text.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+#include<cmath>
+#include<cstdlib>
+using namespace std;
+int main()
+{
+	char b = 'c';
+	cout << b++ << endl;
+	couts << b + 1;
+}
